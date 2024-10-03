bac04f09516411020af4422b953c5e464bebf495
Husrev Akbas <huakbas@c2r3p1.42vienna.com>
2024-10-03 19:21:07 +0200

    all done thanks to michael a lot

--- libft/ft_lstmap_bonus.c
+++ libft/ft_lstmap_bonus.c
@@ -14,34 +14,23 @@
 /* Iterates the list ’lst’ and applies the function ’f’ on the content of each
 node.Creates a new list resulting of the successive applications of the function
 ’f’.The ’del’ function is used to delete the content of a node if needed */
-t_list	*set_new_node(t_list *lst, void *(*f)(void *))
-{
-	t_list	*new_node;
-	void	*new_content;
-
-	new_content = (char *) ft_strdup(lst->content);
-	if (new_content == NULL)
-		return (NULL);
-	new_content = f(new_content);
-	new_node = (t_list *) ft_lstnew(new_content);
-	if (new_node == NULL)
-		return (NULL);
-	return (new_node);
-}
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_list;
 	t_list	*new_node;
+	void	*new_content;
 
 	if (lst == NULL)
 		return (NULL);
 	new_list = NULL;
-	while (lst != NULL)
+	while (lst)
 	{
-		new_node = set_new_node(lst, f);
+		new_content = f(lst->content);
+		new_node = ft_lstnew(new_content);
 		if (new_node == NULL)
 		{
+			del(new_content);
 			ft_lstclear(&new_list, del);
 			return (NULL);
 		}
